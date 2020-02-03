be2489faa7e25930caa42eeb19ac1fb7bcef81a1
khan7149 <faraz.khan@ou.edu>
2020-02-03 11:32:52 -0600

    Didnt work but useful info

---
+++ Wpf-File/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_File
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string book;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FlowDocument fd = new FlowDocument();
+            Paragraph p = new Paragraph();
+            Run text = new Run();
+            File.ReadAllText("Pride and Prejudice by Jane Austen.txt");
+            text.Text = book;
+
+
+            p.Inlines.Add(text);
+            fd.Blocks.Add(p);
+            rbtPride.Document = fd;
+        }
+
+        private void BtnPride_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Environment.GetEnvironmentVariable("USERPROFILE") + @"\Downloads\SalesJan2009.csv";
+
+            var lines = File.ReadAllLines(path);
+
+            //MessageBox
+        }
+    }
+}
